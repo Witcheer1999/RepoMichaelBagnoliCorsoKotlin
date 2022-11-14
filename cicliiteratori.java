d613dd1a8f47be9ddcbe1fd74fac6def5639ef25
Witcheer1999 <113607410+Witcheer1999@users.noreply.github.com>
2022-11-14 16:57:31 +0100

    esercizio cicli iteratori
    Michael Bagnoli 14/11/22, funzionante, può essere migliorato con gli array

---
+++ cicliiteratori.java
@@ -0,0 +1,20 @@
+
+
+public class cicliiteratori {
+        public static void main (String[] args) {
+
+            int x=0;
+
+            while(x<5){
+                System.out.printf("siamo a%2d ripetizioni \n",x);
+                x++;
+            }
+            
+            int y = 0;
+
+            do {System.out.printf("siamo a%2dms senza errori \n",y);
+                y++;
+            } while(y<5);
+
+         }
+    }
